6c7d5f748c57a115cb1fac2cf2a6b7022d0a4f84
“liyanfang <2694154271@qq.com>
2021-05-31 15:10:48 +0800

    [ADD]新增项目

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# vue-d3
-vue+d3 支持 节点点击之后出现数据，  单个节点可以用图片显示，  单个节点可以拖动，  整个页面可以缩放
+ue+d3 支持 节点点击之后出现数据， 单个节点可以用图片显示， 单个节点可以拖动， 整个页面可以缩放
+```vue
+<template>
+  <div id="app">
+    <network :nodeList="nodes" :linkList="links"></network>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  components: {
+    Network
+  },
+  data() {
+    return {
+      nodes: [
+      	{"id": "Myriel", "group": 1},
+      	{"id": "Napoleon", "group": 1},
+        {"id": "Labarre", "group": 2},
+        {"id": "Valjean", "group": 2}
+      ],
+      links: [
+        {"source": "Napoleon", "target": "Myriel", "value": 1},
+        {"source": "Valjean", "target": "Labarre", "value": 1},
+        {"source": "Napoleon", "target": "Valjean", "value": 2},
+      ]
+    };
+  },
+};
+</script>
---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="app">
+    <el-button class="setting-box-button" icon="el-icon-s-operation" circle @click="showSettingCard=!showSettingCard"></el-button>
+    <el-card class="setting-box-card" v-if="showSettingCard">
+      <div slot="header" class="clearfix">
+        <span>设置</span>
+        <el-button
+          style="float: right; padding: 3px 0"
+          type="text"
+          @click="showSettingCard=!showSettingCard"
+        >关闭</el-button>
+      </div>节点大小
+      <el-slider v-model="nodeSize" show-input :max="50"></el-slider>连线宽度
+      <el-slider v-model="linkWidth" show-input :max="20"></el-slider>连线长度
+      <el-slider v-model="linkDistance" show-input :max="300" :step="50" show-stops></el-slider>作用力
+      <el-slider v-model="bodyStrength" show-input :min="-1000" :max="0" :step="50" show-stops></el-slider>
+      <br>
+      <div>
+        <el-switch v-model="svgTheme" active-text="黑暗模式" inactive-text="明亮模式"></el-switch>
+      </div>
+    </el-card>
+    <el-card class="left" v-if="showclickNode">
+      {{ clickNodeval }}
+    </el-card>
+    <network
+      :nodeList="nodes"
+      :linkList="links"
+      :nodeSize="nodeSize"
+      :linkWidth="linkWidth"
+      :linkDistance="linkDistance"
+      :svgTheme="svgTheme?'dark':'light'"
+      :bodyStrength="bodyStrength"
+      @clickNode="clickNode"
+    ></network>
+  </div>
+</template>
+
+<script>
+import Network from "./components/Network.vue";
+import axios from "axios";
+
+export default {
+  name: "app",
+  components: {
+    Network
+  },
+  data() {
+    return {
+      nodes: [],
+      links: [],
+      showSettingCard: false,
+      nodeSize: 14,
+      linkWidth: 2,
+      linkDistance: 50,
+      bodyStrength: -150,
+      svgTheme: false, // light
+      showclickNode: false,
+      clickNodeval: {},
+    };
+  },
+  created() {
+    let url =
+      process.env.NODE_ENV === "production"
+        ? "/vue-network-d3/example.json"
+        : "/example.json";
+    axios
+      .get(url)
+      .then(response => {
+        this.nodes = response.data.nodes;
+        this.links = response.data.links;
+      })
+      .catch(err => console.log(err));
+  },
+  methods: {
+    clickNode(e, val) {
+      this.clickNodeval = val
+      this.showclickNode = true
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.setting-box-button {
+  position: fixed;
+  overflow: auto;
+  top: 20px;
+  right: 40px;
+  border: 2px solid rebeccapurple;
+}
+
+.setting-box-card {
+  position: fixed;
+  z-index: 10;
+  overflow: auto;
+  top: 15px;
+  right: 15px;
+  width: 500px;
+  max-height: 700px;
+}
+.left {
+  width: 300px;  
+  position: absolute;
+  /* display: none; */
+}
+</style>
